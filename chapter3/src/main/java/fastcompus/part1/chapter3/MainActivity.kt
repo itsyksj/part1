623d7e4807da85545fd3cbbd505c94fe711d5be3
iuyt <itsyksj@gmail.com>
2024-04-18 18:22:39 +0900

    feat[chapter3]: Implement unit conversion
    Update activity_main UI.
    Implement unit conversion of MainActivity ImageButton.

--- chapter3/src/main/java/fastcompus/part1/chapter3/MainActivity.kt
+++ chapter3/src/main/java/fastcompus/part1/chapter3/MainActivity.kt
@@ -2,6 +2,8 @@ package fastcompus.part1.chapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
 import fastcompus.part1.chapter3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -30,5 +32,49 @@ class MainActivity : AppCompatActivity() {
 
         //해당 화면에 속하지 않는 id값 (사용불가)
         //val exampleTextView = binding.example
+
+        //기본값 지정
+        var inputNumber: Int = 0
+        var cmToM = true
+
+        //입력값(숫자)은 화면표시를 위해 문자로 변환 후, 출력값을 위해 숫자로 재변환
+        inputNumberEditText.addTextChangedListener { text ->
+
+            //1.예외발생(NumberFormatException) : 입력값이 너무 큰 경우
+            //android:maxLength="7"를 통해 입력값은 7자리 이하로 지정
+
+            //2.예외발생(NumberFormatException) : 입력값이 공백인 경우
+            //입력값을 변환하기 전 공백, null여부 확인시키기
+            inputNumber = if (text.isNullOrEmpty()) {
+                0
+            } else {
+                text.toString().toInt()
+            }
+
+            //3.예외발생(NumberFormatException) : 입력값에 소수점이 들어가는 경우
+            //4.문제점 발생 : 특정 숫자를 입력할 경우 소수점이 과하게 출력됨 (ex) 556cm -> 5.5600000000000005m)
+            Log.d("inputNumber", inputNumber.toString())
+
+            //출력값 설정(cm -> m)
+            if (cmToM) {
+                outputNumberTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputNumberTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
+        //swap버튼을 누른 경우
+        swapImageButton.setOnClickListener {
+            cmToM = cmToM.not()
+            if (cmToM) {
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputNumberTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputNumberTextView.text = inputNumber.times(100).toString()
+            }
+        }
     }
 }
