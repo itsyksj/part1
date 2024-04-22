a660d25448e8760174f5256ac8e16710cf1cc77d
iuyt <itsyksj@gmail.com>
2024-04-22 11:34:14 +0900

    feat[chapter4]: Implement delete data

--- chapter4/src/main/java/fastcompus/part1/chapter4/MainActivity.kt
+++ chapter4/src/main/java/fastcompus/part1/chapter4/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.core.view.isVisible
 import fastcompus.part1.chapter4.databinding.ActivityMainBinding
 
@@ -25,8 +25,14 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        //deleteButton을 눌렀을 때 저장된 데이터 삭제하기
+        binding.deleteButton.setOnClickListener {
+            getDeleteData()
+        }
     }
 
+    //화면이동 발생 시 Lifecycle (EditActivity -> MainActivity)
+    //- EditActivity onPayse() -> MainActivity onResume() -> EditActivity onStop() -> ...
     override fun onResume() {
         super.onResume()
         getDataAndUiUpdate()
@@ -39,7 +45,7 @@ class MainActivity : AppCompatActivity() {
             binding.bodyBirthday.text = getString(BIRTHDATE, "Undefined")
             binding.bodyBloodType.text = getString(BLOOD_TYPE, "Undefined")
             binding.bodyPhoneNumber.text = getString(PHONE_NUMBER, "Undefined")
-            val caution = getString(CAUTION, "Undefined")
+            val caution = getString(CAUTION, "")
 
             //저장된 데이터(주의사항)가 비어있지 않다면 화면에 표시하기
             binding.titleCaution.isVisible = caution.isNullOrEmpty().not()
@@ -50,5 +56,16 @@ class MainActivity : AppCompatActivity() {
                 binding.bodyCaution.text = caution
             }
         }
+    }
+
+    private fun getDeleteData() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE).edit()) {
+            clear()
+            apply()
+            getDataAndUiUpdate()
+        }
+
+        //사용자에게 삭제여부 알리기
+        Toast.makeText(this, "초기화를 완료했습니다", Toast.LENGTH_SHORT).show()
     }
 }
