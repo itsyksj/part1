8b06185df218d9f00a0df3afecad2c44d0cf0296
iuyt <itsyksj@gmail.com>
2024-04-29 15:44:02 +0900

    feat[chapter6]: Implement bottomNavigation
    Specify the start screen for running the app.
    Click navigation to change the screen.
    
    Add Vector for Navigation.
    Update color for Navigation.

--- chapter6/src/main/java/fastcompus/part1/chapter6/MainActivity.kt
+++ chapter6/src/main/java/fastcompus/part1/chapter6/MainActivity.kt
@@ -2,10 +2,42 @@ package fastcompus.part1.chapter6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fastcompus.part1.chapter6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //앱 실행 시에 처음으로 보여질 홈화면(stopwatch) 설정하기
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, StopwatchFragment()).commit()
+        }
+
+        setBottomNavigationView()
+    }
+
+    //네비게이션바에 아이콘을 클릭할 경우 해당 화면으로 이동하기
+    private fun setBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_stopwatch -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container, StopwatchFragment()).commit()
+                    true
+                }
+                R.id.fragment_timer -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container, TimerFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 }
