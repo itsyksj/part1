a702e6cf6816285b1d73ee9f2fc6f18c690a4ee5
iuyt <itsyksj@gmail.com>
2024-05-09 23:16:35 +0900

    fix[chapter6]: Modify Timer startButton function
    Resolve the issue of the Timer restart when stop and start again.
    Code refactored for optimization.

--- chapter6/src/main/java/fastcompus/part1/chapter6/TimerFragment.kt
+++ chapter6/src/main/java/fastcompus/part1/chapter6/TimerFragment.kt
@@ -1,6 +1,8 @@
 package fastcompus.part1.chapter6
 
 import android.app.AlertDialog
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -19,7 +21,7 @@ class TimerFragment : Fragment() {
 
     //타이머를 위한 기본값 지정하기
     private var timerBaseSecond = 60
-    private var currentSecond = 0;
+    private var currentSecond = 0
     private var timer: Timer? = null
 
     override fun onCreateView(
@@ -35,11 +37,7 @@ class TimerFragment : Fragment() {
         //resetButton 설정 (UI도 기본값으로 돌리기)
         binding.resetButton.setOnClickListener {
             reset()
-            Log.d("resetButton", "resetButton 클릭")
-
-            //기본값으로 UI 되돌리기
-            currentSecond = 0
-            binding.timerText.text = "60"
+            Log.d("resetButton", "resetButton 클릭됨")
         }
 
         //startButton 설정
@@ -60,42 +58,48 @@ class TimerFragment : Fragment() {
 
         //타이머 초기값 설정
         binding.timerText.setOnClickListener {
-            tiemrSetting()
+            timerInitSetting()
             Log.d("timerText", "timerText 클릭해 초기값 설정")
         }
     }
 
     private fun reset() {
+        currentSecond = 0
         binding.timerProgressBar.progress = 0
+        binding.timerText.text = timerBaseSecond.toString()
     }
 
     private fun start() {
-        //초시계에 대한 초기값 설정하기
-        currentSecond = timerBaseSecond
+        //초시계 현재값이 지정되지 않았으면 초기값을, 지정되어 있다면 기존값을 사용
+        currentSecond = if (currentSecond == 0) timerBaseSecond else currentSecond
 
-        timer = timer(initialDelay = 1000,  period = 1000) {
+        timer = timer(initialDelay = 1000, period = 1000) {
             currentSecond -= 1
 
-            //초시계 초기값에 따른 프로그래스바 설정하기
+            //프로그래스바 설정
             val progress = ((timerBaseSecond - currentSecond).toFloat() / timerBaseSecond.toFloat() * 100).toInt()
-            Log.d("progress", progress.toString())
 
-            //초가 변경될 때마다 Main Thread를 통해 UI 변경하기
+            //UI 업데이트
             requireActivity().runOnUiThread {
                 binding.timerText.text = String.format("%02d", currentSecond)
+                binding.timerProgressBar.progress = progress
+                Log.d("text", binding.timerText.text.toString())
+                Log.d("progress", progress.toString())
 
-                //초시계의 값이 0이 되는 경우 설정 변경하기 (초기화 되는 경우도 포함)
-                if (currentSecond <= -1) {
+                //초시계 현재값이 0 이하인 경우
+                if(currentSecond <= 0) {
                     pause()
                     binding.startButton.isVisible = true
                     binding.pauseButton.isVisible = false
-                    binding.timerText.text = String.format("%02d", timerBaseSecond)
-                    Toast.makeText(requireContext(), "Timer가 종료되었습니다", Toast.LENGTH_SHORT).show()
-                }
 
-                //초가 줄어들 때마다 프로그래스바 변경하기
-                //TODO(프로그래스바에 애니메이션을 적용시켜 부드럽게 변경시키기)
-                binding.timerProgressBar.progress = progress
+                    //초시계가 종료되면 현재값을 초기값으로 변경
+                    binding.timerText.text = String.format("%02d", timerBaseSecond)
+
+                    //초시계 종료메시지 띄우기
+                    Toast.makeText(requireContext(), "Timer가 종료되었습니다", Toast.LENGTH_SHORT).show()
+
+
+                }
             }
         }
     }
@@ -103,15 +107,16 @@ class TimerFragment : Fragment() {
     private fun pause() {
         timer?.cancel()
         timer = null
+        Log.d("pause", timer.toString())
     }
 
-    private fun tiemrSetting() {
-        //초시계가 실행 중인 경우 초기값 변경불가
-        if(currentSecond == 0) {
-            AlertDialog.Builder(requireContext()).apply {
-                val dialogBinding = DialogTimerSettingBinding.inflate(layoutInflater)
+    private fun timerInitSetting() {
+        //타이머 초기값 지정을 위한 바인딩
+        val dialogBinding = DialogTimerSettingBinding.inflate(layoutInflater)
 
-                //초시계 초기값은 지정된 범위 안(최소값 ~ 최대값 사이)으로 설정
+        if(currentSecond == 0) {
+            //초기값은 최소값 ~ 최대값 사이에서 지정
+            AlertDialog.Builder(requireContext()).apply {
                 with(dialogBinding.timerSetting) {
                     maxValue = 60
                     minValue = 1
@@ -119,11 +124,13 @@ class TimerFragment : Fragment() {
                 }
                 setView(dialogBinding.root)
 
-                //확인을 누르는 경우 초기값 지정하기 (취소를 누르는 경우 null값으로 지정)
+                //확인을 눌렀다면 설정갑을 초기값으로 지정
                 setPositiveButton("확인") { _, _ ->
                     timerBaseSecond = dialogBinding.timerSetting.value
                     binding.timerText.text = String.format("%02d", timerBaseSecond)
                 }
+
+                //취소를 눌렀다면 초기값 없이 null값 지정
                 setNegativeButton("취소", null)
             }.show()
         } else {
