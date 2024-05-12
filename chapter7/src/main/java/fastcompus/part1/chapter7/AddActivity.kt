b94c0e56e175410be42626f87a310247e48979f2
iuyt <itsyksj@gmail.com>
2024-05-12 23:59:48 +0900

    design[chapter7]: Update UI
    Use select only one wordType when add word.

--- chapter7/src/main/java/fastcompus/part1/chapter7/AddActivity.kt
+++ chapter7/src/main/java/fastcompus/part1/chapter7/AddActivity.kt
@@ -2,6 +2,7 @@ package fastcompus.part1.chapter7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.chip.Chip
 import fastcompus.part1.chapter7.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
@@ -11,5 +12,28 @@ class AddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         addBinding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(addBinding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        //wordType 선언
+        val types = listOf("명사", "대명사", "동사", "형용사", "부사", "접속사", "전치사", "감탄사")
+
+        //wordType를 Chip으로 설정
+        addBinding.wordTypeChip.apply {
+            types.forEach { text ->
+                addView(createChip(text))
+            }
+        }
+    }
+
+    //wordType은 1개만 선택하도록 지정
+    private fun createChip(text: String): Chip {
+        return Chip(this).apply {
+            setText(text)
+            isCheckable = true
+            isClickable = true
+        }
     }
 }
